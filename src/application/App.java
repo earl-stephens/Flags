781d78d8e2819b37adfed63d81fbd6d5747856b0
earl-stephens <stephensearl@hotmail.com>
2021-12-20 21:39:42 -0700

    Coded a boolean as a flag

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,35 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
 		
+		//In this case, the boolean is a flag
+		//A flag is usually a binary value used to control
+		//the program
+		
+		boolean gotCodeword = false;
+
+		for (;;) {
+			System.out.print(" > ");
+			String input = scanner.nextLine();
+
+			if (input.equals("quit")) {
+				break;
+			} else if (input.equals("volcano")) {
+				gotCodeword = true;
+			}
+		}
+
+		scanner.close();
+
+		if (gotCodeword) {
+			System.out.println("Codeword entered.");
+		} else {
+			System.out.println("No codeword detected.");
+		}
 	}
 }
